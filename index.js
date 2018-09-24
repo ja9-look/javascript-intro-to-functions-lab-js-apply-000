8e288991fda7494f54fc25c86ad001ca85bf2c4c
Janine Luk <janineluk95@gmail.com>
2018-09-24 19:42:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,11 +18,7 @@ function sayHiToGrandma(string){
   if (string === string.toLowerCase()){
     return `I can\'t hear you!`
   }
-  if (string === string.toUpperCase()){
+  else if (string === string.toUpperCase()){
     return `YES INDEED!`
   }
-}
-
-function sayHiToGrandma(string){
-  
 }
